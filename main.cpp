d4fa955be8aeac49e94bf8cc7873d6192c166ec5
Karnesh Mehra <karneshmehra@gmail.com>
2014-02-13 01:27:38 -0500

    partial register implementation

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ char *myip;
 char *myport;
 char *serverip="localhost";
 char *serverport="8080";
+int connlist[10];
 
 struct addrinfo ai;
 struct sockaddr_storage conns;
@@ -18,6 +19,9 @@ socklen_t conn_size;
 
 // prototypes
 int handleNewConnection();
+void redoFDSET();
+void getData(int);
+void registerWithServer();
 //
 
 void handleCommand(char cmd[20]){
@@ -35,7 +39,7 @@ void handleCommand(char cmd[20]){
 	}
 	else if(!strcmp(cmd, "register")){
 		if(role=='c'){
-
+			registerWithServer();
 		}
 		else{
 			printf("You need to be a client to use this command\n");
@@ -52,6 +56,7 @@ void handleCommand(char cmd[20]){
 	}
 	else if(!strcmp(cmd, "exit")){
 		printf("Exiting the Program, Bye!\n");
+		close(listenfd);
 		exit(1);
 	}
 	else if(!strcmp(cmd, "upload")){
@@ -95,10 +100,8 @@ int makeServer(){
 
 
     while(1){
-    	printf("1\n");
-    	FD_ZERO(&fdreads);
-    	FD_SET(listenfd, &fdreads);
-    	FD_SET(0, &fdreads);
+    	printf("%d**%d\n",maxsock,listenfd);
+   		redoFDSET();
     	int socksel=select(maxsock+1, &fdreads, NULL, NULL, &t);
     	printf("2-%d\n",socksel);
     	if(socksel==-1){
@@ -117,25 +120,122 @@ int makeServer(){
 	    		len=read(0,readbuff,sizeof readbuff);
 	    		readbuff[len-1]='\0';
 	    		if(len>0){
-	    			int a=strcmp("hello",readbuff);
+	    			//int a=strcmp("hello",readbuff);
 	    			//printf("%d-%s]]",a,readbuff);
 	    			handleCommand(readbuff);
+	    		}
+	    	}
+	    	for(int i=0;i<10;i++){
+	    		if(connlist[i]!=-2){
+	    			printf("check for data--%d\n",i);
+	    			if(FD_ISSET(connlist[i], &fdreads)){
+	    				printf("Data incoming\n");
+	    				getData(connlist[i]);
+	    			}
 	    		}
 	    	}
     	}
 
     	
     }
-	// printf("Listening on Port %s\n",port);
-	// conn_size=sizeof conns;
-	// int newfd=accept(s, (struct sockaddr *)&conns, &conn_size);
-	// const char *msg="Hello ji";
-	// char *buf;
-	// while(int recvd=recv(newfd,&buf, 1000, 0) >0){
-	// 	printf("%s",(char *)&buf);
-	// 	int sentb=send(newfd,&msg,strlen(msg),0);
-	// }
 	return 0;
+}
+
+void redoFDSET(){
+	FD_ZERO(&fdreads);
+    FD_SET(listenfd, &fdreads);
+    FD_SET(0, &fdreads);
+	for(int i=0;i<10;i++){
+		if(connlist[i]!=-2){
+			FD_SET(connlist[i], &fdreads);
+		}
+	}
+}
+
+void getData(int fd){
+	char buf[20];
+	read(fd,buf,strlen(buf));
+	//while(int recvd=recv(fd,&buf, 1000, 0) >0){
+		printf("data-%s",buf);
+   //}
+}
+
+int makeClient(){
+	ai.ai_flags=AI_PASSIVE;
+	ai.ai_family=AF_UNSPEC;
+	ai.ai_socktype=SOCK_STREAM;
+
+	int outer=getaddrinfo(NULL,port,&ai,&res);
+	//printf("%d\n", outer);
+	struct sockaddr_in *adr = ( struct sockaddr_in *)res->ai_addr;
+
+	// char ipstr[INET6_ADDRSTRLEN];
+	// inet_ntop(res->ai_family,&(adr->sin_addr), ipstr, sizeof ipstr);
+	listenfd=socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	maxsock=listenfd;
+	int b=bind(listenfd,res->ai_addr,res->ai_addrlen);
+    int l=listen(listenfd,10);
+	
+
+	struct timeval t ;
+	t.tv_sec=5;
+	t.tv_usec=0;
+
+
+    while(1){
+    	printf("%d**%d\n",maxsock,listenfd);
+   		redoFDSET();
+    	int socksel=select(maxsock+1, &fdreads, NULL, NULL, &t);
+    	printf("2-%d\n",socksel);
+    	if(socksel==-1){
+    		perror("There has been select error");
+    	}
+
+    	if(socksel==0){
+    		printf("No nthing\n");
+    	}
+    	if(socksel>0){
+    		if(FD_ISSET(listenfd, &fdreads)){
+    			handleNewConnection();
+	    	}
+	    	if(FD_ISSET(0,&fdreads)){
+	    		int len;
+	    		len=read(0,readbuff,sizeof readbuff);
+	    		readbuff[len-1]='\0';
+	    		if(len>0){
+	    			//int a=strcmp("hello",readbuff);
+	    			//printf("%d-%s]]",a,readbuff);
+	    			handleCommand(readbuff);
+	    		}
+	    	}
+	    	for(int i=0;i<10;i++){
+	    		if(connlist[i]!=-2){
+	    			printf("check for data--%d\n",i);
+	    			if(FD_ISSET(connlist[i], &fdreads)){
+	    				printf("Data incoming\n");
+	    				getData(connlist[i]);
+	    			}
+	    		}
+	    	}
+    	}
+
+    	
+    }
+	return 0;
+}
+
+void addToConnList(int fd){
+	for(int i=0;i<10;i++){
+    	if(connlist[i]==-2){
+    		connlist[i]=fd;
+    		printf("%d=added to conn list==%d\n",i,fd);
+    		if(fd>maxsock){
+    			printf("max bhi tha\n");
+    			maxsock=fd;
+    		}
+    		break;
+    	}
+    }
 }
 
 int handleNewConnection(){
@@ -147,11 +247,33 @@ int handleNewConnection(){
     getnameinfo((struct sockaddr *)&conns, conn_size, buf, sizeof buf, porta, sizeof porta, 0);
     printf("%s--%s\n", buf,porta);
     addConnection(buf, porta);
+    char *msg="got it";
+    //int data=send(newfd,&msg,sizeof msg,0);
+    //printf("%d\n",data );
+    addToConnList(newfd);
     traverseList();
+}
+
+void registerWithServer(){
+	struct addrinfo ai1;
+	ai1.ai_flags=AI_PASSIVE;
+	ai1.ai_family=AF_UNSPEC;
+	ai1.ai_socktype=SOCK_STREAM;
+	struct addrinfo *res1;
+	int outer=getaddrinfo(serverip,serverport,&ai1,&res1);
+	int tmpfd=socket(res1->ai_family, res1->ai_socktype, res1->ai_protocol);
+	int connct=connect(tmpfd,res1->ai_addr, res1->ai_addrlen);
+	char msg[20]="MNCOK";
+	int data=write(tmpfd,msg,strlen(msg));
+	printf("reg with server%lu--%d--%d--%d\n", strlen(msg),tmpfd,connct,data);
+	// /close(tmpfd);
+	
 }
 
 void boot(){
 	initializeList();
+	for(int i=0;i<10;i++)
+		connlist[i]=-2;
 }
 
 int main ( int argc, char *argv[]){
@@ -168,8 +290,7 @@ int main ( int argc, char *argv[]){
 		//printf("Port number is %s\n",port );
 		
 		if(role=='c'){
-
-			//handleCommand();
+			makeClient();
 			//printf("Its a client\n");
 		}
 		else if(role=='s'){
